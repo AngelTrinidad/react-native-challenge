a575cd7ef70323c4278f3633ee377b7671f4d6b0
angel <96trinidad@gmail.com>
2022-01-12 02:19:48 -0300

    refactor(components): change index UI

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,6 @@ export { default as Text } from "./Text";
 export { default as Heading } from "./Heading";
 export { default as ViewFlex } from "./ViewFlex";
 export { default as ViewContainer } from "./ViewContainer";
+export { default as Button } from "./Button";
+export { default as TextInput } from "./TextInput";
+export { default as Card } from "./Card";
