e89a59ad5a7f511d6a36b46185f5c4ed487d8de4
angel <96trinidad@gmail.com>
2022-01-12 02:19:04 -0300

    feat(components): create Card component

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, View, ViewProps } from "react-native";
+import { SHADOW_STYLES } from "../../styles";
+
+type Props = ViewProps & {
+  children?: React.ReactNode;
+};
+
+const Card: React.FC<Props> = ({ children, style, ...rest }) => {
+  return (
+    <View style={[styles.container, style]} {...rest}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "white",
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    borderRadius: 8,
+    ...SHADOW_STYLES
+  },
+});
+
+export default React.memo(Card);
