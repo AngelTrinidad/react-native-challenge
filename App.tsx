3f97bc22142d1cfeb24286ee527247185840fd1f
angel <96trinidad@gmail.com>
2022-01-12 22:20:49 -0300

    refactor: change App component to use navigation

--- App.tsx
+++ App.tsx
@@ -1,37 +1,8 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ToDoScreen from './src/screens/ToDoScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import ListScreen from './src/screens/ListScreen';
-import DetailScreen from './src/screens/DetailScreen';
-import WalletScreen from './src/screens/WalletScreen';
+import * as React from "react";
+import Navigation from "./src/navigation/Navigation";
 
-/**
- * Use `HomeScreen` as the initial route
- * Replace the screen title with the `Logo` component
- *
- * 💯  Usage of TypeScript is a plus
- */
-
-// import Logo from './components/ui/Logo';
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="ToDo" component={ToDoScreen} />
-        <Stack.Screen name="List" component={ListScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
-        <Stack.Screen name="Wallet" component={WalletScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
-}
+const App: React.FC = () => {
+  return <Navigation />;
+};
 
 export default App;
