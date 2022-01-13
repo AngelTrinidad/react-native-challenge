523ca4040bad0109971741d4cd8d5b6229fe5342
angel <96trinidad@gmail.com>
2022-01-12 22:18:54 -0300

    refactor(models): add new model

--- src/models.ts
+++ src/models.ts
@@ -6,6 +6,14 @@ export interface Coin {
   priceUsd: number;
   changePercent24Hr: number;
   supply: number;
-  maxSupply: number;
-  marketCapUsd: number
+  maxSupply: number | null;
+  marketCapUsd: number;
+}
+
+export interface Partner {
+  id: string;
+  name: string;
+  description: string;
+  roles: string;
+  status: string;
 }
