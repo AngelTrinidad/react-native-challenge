b237f321f36980c6e3caffdd77ba99c10436417d
angel <96trinidad@gmail.com>
2022-01-12 22:19:39 -0300

    feat(styles): global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,12 +1,16 @@
-import { ViewStyle } from "react-native";
+import { TextStyle, ViewStyle } from "react-native";
 
 export const THEME = {
   primaryColor: "#1FC4DB",
+  primaryBackgroundColor: "white",
+  secondaryBackgroundColor: "#F8F8FA",
 };
 
 export const DEFAULT_PADDING_HORIZONTAL = 30;
 
 export const DEFAULT_PADDING_TOP = 24;
+
+export const HEADER_PADDING = 12;
 
 export const SHADOW_STYLES: ViewStyle = {
   shadowColor: "black",
@@ -14,7 +18,22 @@ export const SHADOW_STYLES: ViewStyle = {
     width: 0,
     height: 1,
   },
-  shadowOpacity: 0.1,
+  shadowOpacity: 0.125,
   shadowRadius: 1,
   elevation: 10,
+};
+
+export const NAVIGATOR_STYLES: {
+  headerBackTitle: TextStyle;
+  tabbarLabel: TextStyle;
+} = {
+  headerBackTitle: {
+    fontSize: 14,
+    color: THEME.primaryColor,
+  },
+  tabbarLabel: {
+    fontSize: 16,
+    fontWeight: "700",
+    textAlign: "center",
+  },
 };
